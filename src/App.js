b7be47f82f5b570c0301ae45dd208d4b211f391f
Vasia Yurkovets <yurkovets@Vasias-MacBook-Pro.local>
2023-04-05 23:03:48 +0300

    finished task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { CircularProgress, Box } from '@mui/material';
+
+import { fetchData } from './store/students-actions'
+
+import { Main, Details, Notification, PageToggler } from './components'
 
 function App() {
+  const dispatch = useDispatch()
+  const notification = useSelector(state => state.ui.notification)
+  const students = useSelector(state => state.students.students)
+  const columns = useSelector(state => state.students.columns)
+  const rates = useSelector(state => state.students.rates)
+  const isLoaded = students.length !== 0 && columns.length !== 0 && rates.length !== 0
+  const [page, setPage] = useState('1')
+
+  const toggleChange = () => {
+    page === '1' ? setPage('2') : setPage('1')
+  }
+
+  useEffect(() => {
+    dispatch(fetchData('Schoolboy'))
+    dispatch(fetchData('Column'))
+    dispatch(fetchData('Rate'))
+  }, [dispatch])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {notification.open && <Notification {...notification} />}
+      <PageToggler value={page} handleChange={toggleChange} />
+      {!isLoaded &&
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', position: 'absolute' }}>
+          <CircularProgress size={100} />
+        </Box>
+      }
+      {
+        isLoaded && (page === '1'
+          ? <Main />
+          : <Details />
+        )
+      }
     </div>
-  );
+  )
 }
 
 export default App;
